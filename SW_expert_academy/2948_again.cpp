9ef1dbd8d5c73f7c1a582583f3f6aea200fd7e80
so-jin <52068107+so-jin@users.noreply.github.com>
2021-04-07 17:38:08 +0900

    Update 2948_again.cpp

--- SW_expert_academy/2948_again.cpp
+++ SW_expert_academy/2948_again.cpp
@@ -6,17 +6,19 @@ using namespace std;
 
 struct Node {
 	int data;
+	char *origin;
 	Node *left;
 	Node *right;
 };
 
 Node *root = nullptr;
-Node *makeNode(int value) {
+Node *makeNode(int value, char * origin) {
 	Node *newNode = new Node;
 	if (newNode != nullptr) {
 		newNode->data = value;
 		newNode->left = nullptr;
 		newNode->right = nullptr;
+		newNode->origin = origin;
 	}
 	return newNode;
 }
@@ -35,22 +37,22 @@ Node * searchBST(Node *node, int key) {
 	return node;
 }
 
-Node *insertBST(Node *node, int value)
+Node *insertBST(Node *node, int value, char * origin)
 {
 	//삽입하고자 하는 노드로 탐색하고 탐색 실패 위치에 추가한다.
 	if (node == nullptr) {
-		Node *newNode = makeNode(value);
+		Node *newNode = makeNode(value, origin);
 		return newNode;
 	}
 	if (node->data < value) {
-		node->right = insertBST(node->right, value);
+		node->right = insertBST(node->right, value, origin);
 	}
 	else if (node->data > value) {
-		node->left = insertBST(node->left, value);
+		node->left = insertBST(node->left, value,origin);
 	}
 	//중복되는 경우.. 중복 허용 X
 	else {
-		cout << "Duplicate Key\n";
+		//cout << "Duplicate Key\n";
 	}
 	return node;
 }
@@ -62,10 +64,11 @@ unsigned hash_fun(char a[]) {
 	int hashcode = 0;
 
 	for (int j = 0; j < strlen(a); j++) {
-		int tmp = (a[j] - 'a' + 1);
+		int tmp = (a[j]);
 		tmp *= k;
+		k *= 3;
 		hashcode += tmp;
-		k *= 27;
+		
 	}
 	return hashcode;
 }
@@ -74,6 +77,8 @@ int size_, a_size, b_size;
 char* pointer[100000];
 unsigned hashA[100000] = { 0, };
 unsigned hashB[100000] = { 0, };
+char a[100000][50];
+char b[100000][50];
 
 int main() {
 	freopen("sample.txt", "r", stdin);
@@ -84,29 +89,31 @@ int main() {
 		Node *root = nullptr;
 		scanf("%d %d ", &a_size, &b_size);
 
-		char a[50] = { 0, };
-		char b[50] = { 0, };
+//		char a[50] = { 0, };
+	//	char b[50] = { 0, };
 
 		for (int j = 0; j < a_size; j++) {
-			scanf(" %s", b);
+			scanf(" %s", a[j]);
 			//printf("hashA: %d\n", hash_fun(a));
-			hashA[j] = hash_fun(b);
-			root = insertBST(root, hashA[j]);
+			hashA[j] = hash_fun(a[j]);
+			root = insertBST(root, hashA[j], a[j]);
+			//원본 데이터 넣어주기
 
 		}
 
 		for (int j = 0; j < b_size; j++) {
-			scanf(" %s", b);
+			scanf(" %s", b[j]);
 			//printf(" %s", b);
 			//printf("hashB: %d\n", hash_fun(b));
-			hashB[j] = hash_fun(b);
-			if (searchBST(root, hashB[j]) != nullptr) {
+			hashB[j] = hash_fun(b[j]);
+			Node * found = searchBST(root, hashB[j]);
+			if (found != nullptr && strcmp(found->origin, b[j])==0) {
 				same_count++;
 			}
 		}
 
 
-		printf("##%d %d\n", i + 1, same_count);
+		printf("#%d %d\n", i + 1, same_count);
 	}
 
 
